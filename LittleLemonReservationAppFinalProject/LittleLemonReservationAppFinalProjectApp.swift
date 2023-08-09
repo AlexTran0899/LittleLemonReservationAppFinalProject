fac7fdade48a89b9162b22839a930cc50bf889b3
Alextran0899 <alextran0899@gmail.com>
2023-08-08 21:01:23 -0500

    Initial Commit

---
+++ LittleLemonReservationAppFinalProject/LittleLemonReservationAppFinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonReservationAppFinalProjectApp.swift
+//  LittleLemonReservationAppFinalProject
+//
+//  Created by Alex Tran on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonReservationAppFinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
